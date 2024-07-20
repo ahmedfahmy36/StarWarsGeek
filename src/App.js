59985c686754bcabdacdc0d19a94e6397f12c698
ahmedfahmy36 <superbota2000@gamil.com>
2024-07-20 21:53:44 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes,   } from 'react-router-dom';
+import {  Route, Routes,   } from 'react-router-dom';
 import './App.css';
 import SwapiComponent from './components/Characters/Characters';
 import CharacterDetails from './components/charactersdetails/charactersdetails';
